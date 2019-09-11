bc78caea60e70edbde989a6832d7980b97e6295a
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-11 00:39:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,23 @@
 
 def key_for_min_value(name_hash)
   min_value = nil
+  min_item = nil
   
   if name_hash == []
     return nil
   else
-    
+    name_hash.each do |item, value|
+      if min_value == nil
+        min_item = item
+        min_value = value
+      elsif value < min_value
+        min_value = value
+        min_item = item
+      end
+    end
   end
+  
+  min_item
+        
+
 end
