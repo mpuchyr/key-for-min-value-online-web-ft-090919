855c6caa3e760f6ec8f68adbd5a4aede4edf5575
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-11 00:34:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_value = nil
+  
+  name_hash.each do |item, value|
+    if min_value == nil
+      min_value = value
+    elsif min_value < value
+      min_value = value
+    end
+  end
+  
+  name_hash.each do |item, value|
+    if min_value == value
+      return item
+    end
+  end
 end
